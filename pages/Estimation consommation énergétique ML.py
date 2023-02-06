0ecfe28fc4e746a7f3aaec7332b1312f0b724560
kevin <k.osanlou@gmail.com>
2023-02-06 15:14:04 +0100

    added server conf

--- pages/Estimation consommation énergétique ML.py
+++ pages/Estimation consommation énergétique ML.py
@@ -2,39 +2,71 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
+from PIL import Image
 
-# Charger le modèle pré-entraîné
+# Load the pre-trained model
 loaded_model = pickle.load(open("best_model_2.pkl", "rb"))
 
-# Charger le scaler
+# Load the scaler
 with open('scaler_2.pkl', 'rb') as f:
     scaler, column_names = pickle.load(f)
 
+# Bold and italic title
 
-# Titre en gras et italique
 st.set_page_config(page_title="Consommation énergétique Modèle de ML", page_icon=":guardsman:", layout="wide")
+
+st.markdown(
+    """
+    <style>
+        body {
+            background-color: #2b2d42;
+        }
+        h1 {
+            text-align: center;
+            color: #2b2d42;
+
+            border-color: #f2f2f2;
+        }
+        .number-input-container {
+            width: 50%;
+            color: #f2f2f2;
+        }
+        input[type='number'] {
+            color: #f2f2f2;
+            background-color: #5e5e5e;
+            border-color: #5e5e5e;
+        }
+        .stButton {
+            background-color: #f2f2f2;
+            color: #2b2d42;
+            border-color: #f2f2f2;
+        }
+    </style>
+
+
+    """,
+    unsafe_allow_html=True,
+)
+
 st.title("Consommation énergétique")
 
-# Inputs de l'utilisateur
-input1 = st.number_input("Taille Dataset", format='%d', step=1)
-input2 = st.number_input("Nombre de Batch", format='%d', step=1)
-input3 = st.number_input("Nombre de parametres", format='%d', step=1)
+input1 = st.number_input("Taille Dataset", value=0, step=1000)
+input2 = st.number_input("Nombre de Batch", value=0, step=2)
+input3 = st.number_input("Nombre de parametres", value=0, step=10000)
 
-input1 = int(input1)
-input2 = int(input2)
-input3 = int(input3)
+input1 = float(input1)
+input2 = float(input2)
+input3 = float(input3)
 
-# Normaliser les inputs de l'utilisateur avec le scaler
 new_data = pd.DataFrame([[input1, input2, input3]], columns=column_names)
 inputs_scaled = scaler.transform(new_data)
 
-# Stocker les inputs de l'utilisateur pour optimiser les performances
+
 @st.cache
 def get_prediction(inputs_scaled):
     return loaded_model.predict(inputs_scaled)
 
-# Bouton pour calculer la prédiction
+
 if st.button("Calculer"):
     prediction = get_prediction(inputs_scaled)
-    st.write("La consommation énergétique sera de", prediction[0])
-
+    st.write("La consommation énergétique sera de ", round(prediction[0], 2), " mwh")
